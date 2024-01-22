5b855c227b5766ed8bf95e5d9341f1dd84063eac
Niko Mastakas <nicolasmastakas@gmail.com>
2024-01-22 12:32:52 -0300

    refactor: remove error boundary

--- App.tsx
+++ App.tsx
@@ -3,23 +3,15 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Provider } from 'react-redux'
-import ErrorBoundary from 'react-native-error-boundary'
+
 import { store } from './src/store/store'
 import Home from './src/pages/Home/Home'
 import EventDetailComponent from './src/pages/ExhibitionDetail/EventDetail'
 import Favorites from './src/pages/Favorites/Favorites'
-import { Text, View } from 'react-native'
 
 const HomeStack = createStackNavigator()
 const FavoritesStack = createStackNavigator()
 const Tab = createBottomTabNavigator()
-
-const ErrorFallback = ({ error }) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 18, marginBottom: 10 }}>Oops, something went wrong!</Text>
-    <Text style={{ color: 'red' }}>{error.toString()}</Text>
-  </View>
-)
 
 const HomeStackScreen = () => (
   <HomeStack.Navigator>
@@ -34,20 +26,15 @@ const FavoritesStackScreen = () => (
   </FavoritesStack.Navigator>
 )
 
-const App = () => {
-  const [isErrorComponentVisible, setIsErrorComponentVisible] = React.useState(false)
-  return (
-    <Provider store={store}>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeStackScreen} />
-            <Tab.Screen name="Favorites" component={FavoritesStackScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </ErrorBoundary>
-    </Provider>
-  )
-}
+const App = () => (
+  <Provider store={store}>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  </Provider>
+)
 
 export default App
